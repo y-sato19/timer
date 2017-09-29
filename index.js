44f0a5c6437e2ff7d6f0470ae11745aeae2bd0f1
Yusuke Sato @brain <sato-y@brain-net.co.jp>
2017-09-29 19:39:43 +0900

    タイマー作りました。ver1.0

---
+++ index.js
@@ -0,0 +1,42 @@
+"use strct";
+
+// Electronのモジュール
+const electron = require("electron");
+
+// アプリケーションをコントロールするモジュール
+const app = electron.app;
+
+// ウィンドウを作成するモジュール
+const BrowserWindow = electron.BrowserWindow;
+
+var Toaster = require('electron-toaster');
+var toaster = new Toaster();
+
+// メインウィンドウはGCされないようにグローバル宣言
+let mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on("window-all-closed", () => {
+  if (process.platform != "darwin") {
+    app.quit();
+  }
+});
+
+
+// Electronの初期化完了後に実行
+app.on("ready", () => {
+  //ウィンドウサイズを1280*720（フレームサイズを含まない）に設定する
+  mainWindow = new BrowserWindow({width: 800, height: 600, useContentSize: true, transparent: false, frame: true});
+  toaster.init(mainWindow);
+  //使用するhtmlファイルを指定する
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+});
+
+exports.exampleRemote = function () {
+    console.log('main process');
+};
